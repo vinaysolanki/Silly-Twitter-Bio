0dda739ffa9f356642883a85faef4618b3eac111
vinay <vinaysolanki2005@yahoo.co.in>
2012-06-10 18:12:17 -0700

    my first commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,7 +16,13 @@ class HomeController < ApplicationController
 
     #@bio << @role[rand(@role.length)].value
     @bio << @title[rand(@title.length)].value + ", " + @desc[rand(@desc.length)].value + ", " + @role[rand(@role.length)].value + ". " + @phrase[rand(@phrase.length)].value
-    #@final = @bio[0]
+    @count = @bio[0]
+    if @count.length < 140
+      @final = @bio[0]
+    else
+      random_bio
+    end
+
     #@bio.gsub(([]),"")
     #bio << FunnyShit[rand(fs.length)]
     #bio << FunnyShit[rand(fs.length)] # I don't grab the same one that I did before
